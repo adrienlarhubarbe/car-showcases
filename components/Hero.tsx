f92faea798f5b435d6c5769817cfa6c14b1b65da
Jean Adrien LARHUBARBE <alarhubarbe@Prodigious.mu>
2023-06-19 14:44:35 +0400

    First commit

---
+++ components/Hero.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Image from "next/image";
+import CustomButton from "./CustomButton";
+
+const Hero = () => {
+  const handleScroll = () => {};
+  return (
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Quickly and easily find, reserve, or hire a car!
+        </h1>
+        <p className="her__subtitle">
+          With our simple booking process, you can streamline your experience
+          renting a car.
+        </p>
+
+        <CustomButton
+          title="Explore Cars"
+          containerStyles="bg-primary-blue text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
+      </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image src="/mini.png" alt="hero" fill className="object-contain" />
+        </div>
+        <div className="hero__image-overlay" />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
